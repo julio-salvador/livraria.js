970d5a4e4d0d8dba3a9589ca1d1907011e52d792
Julio Salvador Girardi <julio.girardi@escola.pr.gov.br>
2026-05-11 17:39:49 +0000

    feito.

---
+++ script.js
@@ -0,0 +1,11 @@
+let precoLivros = [25, 15, 50, 100, 120, 110, 55];
+
+let maisBarato = 0;
+
+for (let atual = 0; atual < precoLivros.length; atual++){
+    if (precoLivros[atual] < precoLivros[maisBarato]){
+        maisBarato = atual;
+    }
+}
+
+console.log(`O livro mais barato custa: ${precoLivros[maisBarato]} R$`);
